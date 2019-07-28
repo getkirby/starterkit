83d6321975904a91027e70695d93cc59e00073be
Sonja Broda <sonja@imap.cc>
2019-07-28 16:03:52 +0200

    Update template annotations

--- site/templates/album.php
+++ site/templates/album.php
@@ -2,6 +2,8 @@
 /**
  * Templates render the content of your pages. 
  * They contain the markup together with some control structures like loops or if-statements.
+ * The `$page` variable always refers to the currently active page. 
+ * To fetch the content from each field we call the field name as a method on the `$page` object, e.g. `$page->title()`. 
  * This example template makes use of the `$gallery` variable defined in the `album.php` controller 
  * and the `cover()` method defined in the `album.php` page model.
  * Snippets like the header and footer contain markup used in multiple templates. They also help to keep templates clean.
--- site/templates/photography.php
+++ site/templates/photography.php
@@ -2,6 +2,8 @@
 /**
  * Templates render the content of your pages. 
  * They contain the markup together with some control structures like loops or if-statements.
+ * The `$page` variable always refers to the currently active page. 
+ * To fetch the content from each field we call the field name as a method on the `$page` object, e.g. `$page->title()`.
  * This template lists all all the subpages of the `phototography` page with title and cover image.
  * Snippets like the header, footer and intro contain markup used in multiple templates. They also help to keep templates clean.
  * More about templates: https://getkirby.com/docs/guide/templates/basics
