0201a2187755a8c670a83b3248b218c8ef14bd2a
Sonja Broda <sonja@imap.cc>
2019-07-28 16:24:01 +0200

    Update header annotations

--- site/snippets/header.php
+++ site/snippets/header.php
@@ -2,7 +2,7 @@
 /**
  * Snippets are a great way to store code snippets for reuse or to keep your templates clean.
  * This header snippet is reused in all templates. 
- * It fetches information from the `site.txt` content file and contains the menu.
+ * It fetches information from the `site.txt` content file and contains the site navigation.
  * More about snippets: https://getkirby.com/docs/guide/templates/snippets
  */
 ?>
@@ -14,8 +14,11 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width,initial-scale=1.0">
 
+  <!-- The title tag we show the title of our site and the title of the current page -->
   <title><?= $site->title() ?> | <?= $page->title() ?></title>
 
+  <!-- Stylesheets can be included using the `css()` helper. Kirby also provides the `js()` helper to include script file. 
+        More Kirby helpers: https://getkirby.com/docs/reference/templates/helpers -->
   <?= css(['assets/css/index.css', '@auto']) ?>
 
 </head>
@@ -23,6 +26,7 @@
 
   <div class="page">
     <header class="header">
+      <!-- In this link we call `$site->url()` to create a link back to the homepage -->
       <a class="logo" href="<?= $site->url() ?>"><?= $site->title() ?></a>
 
       <nav id="menu" class="menu">
